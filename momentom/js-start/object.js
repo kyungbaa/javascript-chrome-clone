fea2a39d4bb7a31e5a7308c0a12b244ab4675f1d
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-01-10 17:00:25 +0900

    funtion

--- momentom/js-start/object.js
+++ momentom/js-start/object.js
@@ -11,32 +11,20 @@ const player = ["kyungBa", 121212, true, "little bit"];
 
 */
 
+//object는 property를 가진 데이터를 저장해주며, { } 를 사용한다.
 const player = {
-  name: "KyungBaa",
+  name: "Kyung Baa",
   points: 10,
   handsome: true,
   fat: "little bit",
 };
 console.log(player);
+
+//property를 불러오는 방법은 2가지가 있다.
 console.log(player.name);
-
-/**
- * 
- * object는 property를 가진 데이터를 저장해주며, { } 를 사용한다.
-
-const player = {
-name : tomato,
-color : red,
-food : true,
-};
-
+console.log(player["name"]);
 console.log(player);
-
-property를 불러오는 방법은 2가지가 있다.
-
-1. console.log(player.name); => tomato
-2. console.log(player["name"]); => tomato
-
+/*
 또한 property를 바꾸는 것은 가능하지만 선언된 object를 바꾸는 것은 불가능하다.
 ex)
 const player = {
@@ -44,16 +32,9 @@ name : tomato,
 color : red,
 food : true,
 };
-
+*/
+player.handsome = false; //속성값 수정
+player.points = player.points + 2;
+player.lastName = "potato"; // 속성값 추가
 console.log(player);
-player.color = "blue";
-console.log(player.color);
---> blue
-
-그리고 property를 추가 할 수도 있다.
-
-player.koreanName = "토마토";
-
---> {name: "tomato", color: "blue", food: true, koreaName: "토마토"}
- *
- */
+// ** const는 let과 다르게 update가 안되지만 리스트의 경우 전체를 변경하는 게 아니라 속성값을 수정/추가하는 경우에는 update 가 가능하다
