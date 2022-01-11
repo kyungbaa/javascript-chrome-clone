fc7c085134ec74894ee1de84f74d199592ce648e
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-01-11 20:46:01 +0900

    Start
    monentom inout start

---
+++ momentom/app.js
@@ -0,0 +1,9 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit() {
+  const userName = loginInput.value;
+  console.log(userName);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
--- momentom/js-start/event.js
+++ momentom/js-start/event.js
@@ -5,46 +5,55 @@ console.log(h1.id);
 console.log(h1.className);
 */
 
+/*
 const h1 = document.querySelector("div.hello:first-child h1");
-
-h1.innerText = "Hell0o0o!";
-h1.style.color = "white";
-
-function handleh1Click() {
-  h1.innerText = ">_<";
-  console.log("h1 was clicked!");
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMousenter() {
-  h1.innerText = "Mouse is Here! : )";
-}
-function handleMouseLeave() {
-  h1.innerText = "Mouse is Gone! : (";
-}
+h1.addEventListener("click", handleTitleClick);
+*/
 
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
+/*
+
+classList 우리가 class들의 목록으로 작업할수 있게끔 허용해준다.
+className은 이전calss를 상관하지않고 모든걸 교체해 버린다.
+
+classList를 이용하는건
+js에서 건드리는건 HTML element가 가지고있는 또하나의 요소 사용하는 것이다.
+= element의 class내용물을 조작하는 것을 허용한다는 뜻
+
+contains은 우리가 명시한 class가 HTML element의 class에 포함되어 있는지 말해준다
+
+toggle은 토큰이 존재하면 토큰제거
+토큰존재 하지않으면 토큰 추가
+
+*/
+
+/*
+const h1 = document.querySelector("div.hello:first-child h1");
+function handleTitleClick() {
+  const clickedClass = "clicked";
+  // clickedclass가  h1의 classList에 포함되어있다면
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
 }
+h1.addEventListener("click", handleTitleClick);
+*/
 
-function handleCopy() {
-  alert("copppppy!!");
+// toggle
+const h1 = document.querySelector("div.hello:first-child h1");
+function handleTitleClick() {
+  h1.classList.toggle("clicked");
 }
-
-function handleWindowOffline() {
-  alert("SOO no WIFI!!");
-}
-
-function handleWindowONline() {
-  alert("ALL Goooood!!");
-}
-
-h1.addEventListener("click", handleh1Click);
-//h1.onclick = handleh1Click;
-h1.addEventListener("mouseenter", handleMousenter);
-//h1.onmouseenter = handleMousenter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowONline);
+h1.addEventListener("click", handleTitleClick);
