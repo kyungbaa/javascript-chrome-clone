8799ae1ba47ffa7d4f741c8595896b3bcab308f2
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-01-12 12:48:33 +0900

    Start quoto

---
+++ momentom/js/quoto.js
@@ -0,0 +1,61 @@
+const quoto = [
+  {
+    quoto:
+      "Laughter is timeless, Imagination has no age. And dreams are forever.",
+    author: "Walt Disney",
+  },
+  {
+    quoto:
+      "The past can hurt, but the way I see it, you can either run from it or learn from it.",
+    author: "Lion King",
+  },
+  {
+    quoto:
+      "You must not let anyone define your limits because of where you come from. Your only limit is you soul.",
+    author: "Ratatouille",
+  },
+  {
+    quoto: "Today's special moments are tomorrow's memories.",
+    author: "Aladdin",
+  },
+  {
+    quoto: "Sometimes the smallest things take up the most room in your heart.",
+    author: "Winnie the Pooh",
+  },
+  {
+    quoto:
+      "It's not until you lose everything that you can truly appreciate everything.",
+    author: "Beauty and the beast",
+  },
+  {
+    quoto:
+      "The only way to get what you want in this world is through hard work.",
+    author: "The princess and the Frog",
+  },
+  {
+    quoto: "Venture outside your comfort zone. The rewards are worth it.",
+    author: "Tangled",
+  },
+  {
+    quoto:
+      "Our fate lives within us, you only have to be brave enough to see it.",
+    author: "Brave",
+  },
+  {
+    quoto: "You control your destiny. You don't need magic to do it.",
+    author: "Brave",
+  },
+  {
+    quoto:
+      "Never say good bye because goodbye means going away and going away means forgetting.",
+    author: "Peter Pan",
+  },
+  {
+    quoto: "The very things that hold you down are going to lift you up.",
+    author: "Dumbo",
+  },
+  {
+    quoto: "Sometimes the right path is not the easiest one.",
+    author: "Pocahontas",
+  },
+];
