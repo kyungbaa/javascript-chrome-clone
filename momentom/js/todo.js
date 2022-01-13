8c9bed84cbef289b61f4c287c15d264a09e115b0
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-01-13 15:13:55 +0900

    add ToDo Save

--- momentom/js/todo.js
+++ momentom/js/todo.js
@@ -1,13 +1,18 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
 // toDo 저장 리스트 array
 const toDos = [];
 
 // toDosArray의 내용을 localStorange에 넣기
-function saveTodos() {
-  localStorage.setItem("todo", toDos);
+function saveToDos() {
+  localStorage.setItem("TODOS_KEY", JSON.stringify(toDos));
 }
+//json.stringify > 나중에 json.parse 함수를 사용하기 때문임.
+//(STRINGIFY = 변수 등을 문자열로 바꿈, PARSE = 문자열을 JSON으로 바꿈)
 
 function deleteTodo(event) {
   const li = event.target.parentElement; // 클릭한 버튼의 부모 찾기
@@ -33,7 +38,19 @@ function handleToDoSubmit(event) {
   toDoInput.value = "";
   toDos.push(newTodo); // newTodo를 toDos list에 추가
   paintToDo(newTodo);
-  saveTodos();
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+/*
+localStorage.getItem("todos");
+>> '["1","2","3","4"]' 단순한 String 형태
+JSON.parse(localStorage.getItem("todos"));
+>> (4) ['1', '2', '3', '4'] String을 배열로 바꿈
+*/
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+}
