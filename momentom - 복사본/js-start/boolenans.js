703ac91b010f780751ff611d8dcd413d289a7184
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-01-14 17:02:31 +0900

    css

--- momentom - 복사본/js-start/boolenans.js
+++
@@ -1,7 +0,0 @@
-const amIFat = false;
-console.log(amIFat);
-let someting;
-const B = null;
-
-console.log(B); //null=값이 없음
-console.log(someting); //undefined=값이 정의되지 않음
--- momentom - 복사본/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
-
-const image = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${image}`;
-
-document.body.appendChild(bgImage);
